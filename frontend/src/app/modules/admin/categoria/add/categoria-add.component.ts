9b84284d86fcc8d55e76e20bdf4c78eecf45ae9c
Weslen <weslen1905@gmail.com>
2022-07-14 12:34:34 -0300

    Adaptação para o novo sistema de toast e refatoração

--- frontend/src/app/modules/admin/categoria/add/categoria-add.component.ts
+++ frontend/src/app/modules/admin/categoria/add/categoria-add.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { NgToastService } from 'ng-angular-popup';
 import { CategoriaService } from 'src/app/core/services/categoria.service';
 
 @Component({
@@ -15,38 +14,26 @@ export class CategoriaAddComponent implements OnInit {
   constructor(
     private _formBuilder: FormBuilder,
     private _categoriaService: CategoriaService,
-    private _router: Router,
-    private _toastService: NgToastService) { }
-
-  ngOnInit(): void {
+    private _router: Router
+  ) {
     this.form = this._formBuilder.group({
       nome: ['', [Validators.required, Validators.minLength(4)]],
       slug: ['', [Validators.required, Validators.minLength(4)]]
     });
   }
 
+  ngOnInit(): void { }
+
   onSubmit(): void {
     this.isAdding = true;
     if (this.isAdding) this._categoriaService.addCategoria(this.form.value)
       .subscribe(
-        (_) => {
-          this._router.navigateByUrl('/categorias');
-          this._toastService.success({
-            detail: 'Categoria adicionada',
-            summary: `${this.form.controls['nome'].value} foi adicionada com sucesso \n`,
-            position: 'tr'
-          });
-        },
-        (err) => {
-          if (!!err.error.nome) {
-            if (err.error.nome.invalid) this.form.controls['nome'].setErrors({ 'invalid': true });
-          }
-
-          if (!!err.error.slug) {
-            if (err.error.slug.invalid) this.form.controls['slug'].setErrors({ 'invalid': true });
-          }
+        {
+          next: (_) => this._router.navigateByUrl('/categorias'),
+          error: (err) => Object.keys(err.error).forEach((key: any) => this.form.controls[key].setErrors(err.error[key])),
+          complete: () => this.isAdding = false
         }
       )
-      .add(() => this.isAdding = false);
+
   }
 }
--- frontend/src/app/modules/admin/categoria/list/list-categoria.component.ts
+++ frontend/src/app/modules/admin/categoria/list/list-categoria.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { NgToastService } from 'ng-angular-popup';
 import { CategoriaService } from 'src/app/core/services/categoria.service';
 import { ICategoria } from 'src/app/core/types/categoria.types';
 import Swal from 'sweetalert2';
@@ -12,60 +11,32 @@ export class ListCategoriaComponent implements OnInit {
 
   categorias!: ICategoria[];
 
-  constructor(private _categoriaService: CategoriaService, private _toastService: NgToastService) { }
+  constructor(private _categoriaService: CategoriaService) { }
 
   ngOnInit(): void {
-    this._categoriaService.getAllData().subscribe(
-      (data) => {
-        this.categorias = data
-      },
-      (_) => this._toastService.error({
-        detail: 'Falha ao carregar categorias',
-        summary: "Atualize a página para tentar novamente",
-        position: 'tr'
-      }
-      ));
-
+    this._categoriaService.getAllData().subscribe((categorias) => this.categorias = categorias)
   }
 
   delCategoria(id: string, nome: string) {
-
-    Swal
-      .mixin({
-        customClass: {
-          confirmButton: 'btn btn-success mx-1 fs-5',
-          cancelButton: 'btn btn-danger mx-1 fs-5'
-        },
-        buttonsStyling: false
-      })
-      .fire({
-        title: 'Essa ação é irreversível',
-        icon: 'warning',
-        html: `<span class="fs-5">Deseja realmente deletar a categoria <strong>${nome}</strong></span>`,
-        showCancelButton: true,
-        cancelButtonText: 'Cancelar',
-        confirmButtonText: 'Deletar',
-        reverseButtons: true
-      })
+    Swal.fire({
+      customClass: {
+        confirmButton: 'btn btn-success mx-1 fs-5',
+        cancelButton: 'btn btn-danger mx-1 fs-5'
+      },
+      buttonsStyling: false,
+      icon: 'warning',
+      html: `<span class="fs-3">Deseja realmente deletar a categoria <b>${nome}</b></span>
+      <br>
+      <small>*As postagens associadas a essa categoria também serão removidas</small>`,
+      showCancelButton: true,
+      cancelButtonText: 'Cancelar',
+      confirmButtonText: 'Deletar',
+      reverseButtons: true
+    })
       .then((result) => {
         if (result.isConfirmed) {
-
-          this._categoriaService.deleteCategoriaById(id).subscribe(
-            () => {
-              Swal.fire('Deletado com sucesso!', '', 'success');
-
-              this.categorias = this.categorias?.filter((categorias) => {
-                if (categorias._id === id) {
-                  return false
-                } else {
-                  return true;
-                }
-              })
-            },
-            (_) => {
-              Swal.fire('Erro', `<span class='fs-5'>Não conseguimos deletar a categoria <strong>${nome}</strong>, tente novamente mais tarde</span>`, 'error');
-            }
-          );
+          this._categoriaService.deleteCategoriaById(id)
+            .subscribe(() => this.categorias = this.categorias?.filter((categoria) => categoria._id !== id));
         }
       })
 
